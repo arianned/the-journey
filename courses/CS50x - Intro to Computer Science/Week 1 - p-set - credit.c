b5c4c8c63ef025a0ec9796e596039878513ddd2d
arianned <ariannedonoghue@gmail.com>
2017-06-24 21:10:08 +0100

    uploaded completed solution to pset1 credit problem

--- courses/CS50x - Intro to Computer Science/Week 1 - p-set - credit.c
+++ courses/CS50x - Intro to Computer Science/Week 1 - p-set - credit.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 
-
-int main()
+int main (void)
 {
-    long long credit;           // credit card number
-    long long len;              // used to work out len of card number
-    int count = 0;              // count to work out credit card number
-    long long ccmulti;          // credit card number used for addition sum
-    int pen;                    // penultimate digit
-    int multi = 0;              // sum of alternate digits, starting with penultimate
-    long long ccadd;            // credit card number used for addition sum
-    int end;                    // end digit
-    int add = 0;                // added digits
-    int checksum;               // combo of multi and add
+    // prompt user to enter credit card number and validate that it's a positive number
     
-    
-    // get credit card number from user and validate it's a positive integer
+    long long credit;               // credit card number
     do
     {
         printf("Please enter the credit card number you wish to validate: ");
@@ -24,31 +13,59 @@ int main()
     }
     while (credit <= 0);
     
-    // validate length of credit card number and get length of card
-
-    len = credit;
-    while (len > 0)
+    // get length of credit card number
+    
+    long long cclen;                        // variation of the credit card number used to find the length
+    int len = 0;                            // the length of the credit card number
+    cclen = credit;
+    while (cclen > 0)
     {
-        len = len / 10;
-        count ++;
+        cclen /=10;
+        len ++;
     }
     
-    // select and multiply alternate digits, beginning with pentultimate
-    ccmulti = credit;
+    // get starting digits of credit card number
     
-    ccmulti = ccmulti / 10;
+    long long visa = credit;
+    long long mc = credit;
+    long long amex = credit;
+    
+    while (visa >= 10)
+    {
+        visa /= 10;
+    }
+    
+    while (mc >= 100)
+    {
+        mc /= 10;
+    }
+    
+    while (amex >= 100)
+    {
+        amex /= 10;
+    }
+    
+    // multiply every other digit, starting with the penultimate - find penultimate digit and multiply it by 2 unless sum >10. If sum >=10, add the products together
+    
+    long long ccmulti = credit;             // variation of the credit card number, used to fetch alternate digits
+    int pen;                                // penultimate digit - setting this up over next two rows
+    ccmulti = ccmulti /10;                  
     pen = ccmulti % 10;
+    int multi = 0;                           // sum of multiplied digits
     if (pen * 2 < 10)
     {
         multi = multi + (pen * 2);
     }
     else
     {
-        multi = multi + ((pen * 2) % 10)+1;
+        multi = multi + (((pen * 2) % 10) + 1);
     }
-    for (int j = 0; j <= count; j++)
+    
+    // now move back to every other digit, multiplying by 2 unless sum >10. If sum >10, add the products together
+    
+    for (int i = 0; i <= (len-1); i+= 2)
     {
-        ccmulti = ccmulti / 100;
+        ccmulti /= 100;
         pen = ccmulti % 10;
         if (pen * 2 < 10)
         {
@@ -56,33 +73,49 @@ int main()
         }
         else
         {
-            multi = multi + ((pen * 2) % 10)+1;
+            multi = multi + (((pen * 2) % 10) + 1);
         }
-    } 
-    
-    // select and add alternate digits, beginning with last one
-    ccadd = credit;
-    for (int i = 0; i <= count; i++)
-    {
-        end = ccadd % 10;
-        ccadd = ccadd / 100;
-        add = add + end;
     }
-
-    // combine add and multi for checksum
-
-    checksum = (multi + add) % 10;
+    
+    // now select and add remaining digits (counting back from the final digit, two at a time)
+    
+    long long ccadd = credit;               // variation of the credit card number, used to fetch alternate digits
+    int final;                              // final digit - setting this up over next two rows
+    int add = 0;                            // sum of added digits
+    for (int j = 0; j <= len; j+= 2)
+    {
+        final = ccadd % 10;
+        add = add + final;
+        ccadd /=100;
+    }
+    
+    // now combine both sets of numbers to create a checksum
+    
+    int checksum = multi + add;
+    
+    // begin validation of credit card number
+    
+    // validate checksum
+    
     if (checksum % 10 != 0)
     {
         printf("INVALID\n");
     }
+    // if checksum = 0, check 
+    else if ((visa == 4) && (len == 13 || len == 16))
+    {
+        printf("VISA\n");
+    }
+    else if ((amex == 34 || amex == 37) && (len == 15))
+    {
+        printf("AMEX\n");
+    }
+    else if ((mc == 51 || mc == 52 || mc == 53 || mc == 54 || mc == 55) && (len ==16))
+    {
+        printf("MASTERCARD\n");
+    }
     else
     {
-     printf("%i\n", checksum); 
+        printf("INVALID\n");
     }
-    
-    
-    
-    return 0;
 }
-
